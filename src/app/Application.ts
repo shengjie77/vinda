22167ebcee7e81f813436a389ef8e50244c37aa8
ShengJie <shengjie0202@outlook.com>
2021-04-03 10:17:28 +0800

    Add canvas

--- src/app/Application.ts
+++ src/app/Application.ts
@@ -7,6 +7,26 @@ export class Application {
 
 	public run() {
 		console.log('Application is running...');
+		this.test();
 	}
 
+	// ------------------------------------------------------- //
+	// ---------------  Private Section Below  --------------- //
+	// ------------------------------------------------------- //
+
+	private test() {
+		const container = document.getElementById('container') as HTMLElement;
+		// mountUI(container);
+		const canvas = document.getElementById('canvas') as HTMLCanvasElement
+		fillScreen(canvas);
+
+	}
+
+}
+
+function fillScreen(canvas: HTMLCanvasElement) {
+	canvas.style.height = '100vh';
+	const ratio = window.devicePixelRatio;
+	canvas.width = canvas.clientWidth * ratio;
+	canvas.height = canvas.clientHeight * ratio;
 }
