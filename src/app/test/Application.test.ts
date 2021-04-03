0192f98b3ee88a69022241294fe5e16961b4e25f
ShengJie <shengjie0202@outlook.com>
2021-04-03 11:29:17 +0800

    test

--- src/app/test/Application.test.ts
+++ src/app/test/Application.test.ts
@@ -1,7 +1,7 @@
 import { Application } from 'src/app/Application';
 
 describe('test', () => {
-	test('aa', () => {
+	test('bb', () => {
 		const app = Application.create();
 		expect(app instanceof Application).toBeTruthy();
 	})
